5341c896e3fe7e59eefae2207bceecfe638fc240
Jimmy <baker.jimmy@gmail.com>
2021-09-08 14:39:10 -0400

    Challege Completed!

---
+++ assign02-inheritance/challenge_03.php
@@ -0,0 +1,72 @@
+<?php
+
+class Bicycle {
+
+  public $brand;
+  public $model;
+  public $year;
+  public $description = 'Used bicycle';
+  private $weight_kg = 0; // unusable by unicycle
+  protected $wheels = 2;
+
+  // Getters
+  public function name() {
+    return "$this->brand $this->model ($this->year)";
+  }
+  
+  public function weight_kg() {
+    return "$this->weight_kg kg";
+  }
+  
+  public function weight_lbs() {
+    $weight_lbs = floatval($this->weight_kg) * 2.2046226218;
+    return "$weight_lbs lbs";
+  }
+  
+  public function wheel_details() {
+    $is_wheels = ($this->wheels == 1) ? "It has only 1 wheel." : "It has $this->wheels wheels.";
+    return $is_wheels;
+  }
+  
+  //Setters
+  public function set_weight_kg($value) {
+    $this->weight_kg = floatval($value);
+  }
+  
+  public function set_weight_lbs($value) {
+    $this->weight_kg = floatval($value) / 2.2046226218;
+  }
+
+}
+
+class Unicycle extends Bicycle {
+  protected $wheels = 1;
+}
+
+$lekker = new Bicycle;
+$lekker->brand = 'Lekker';
+$lekker->model = 'Jordaan';
+$lekker->year = '2020';
+
+$uni = new Unicycle;
+
+echo "<p>";
+echo "Bicycle: " . $lekker->wheel_details() . "<br>";
+echo "Unicycle: " . $uni->wheel_details() . "<br>";
+echo "</p>";
+
+echo "<p>";
+echo "Set weight using kg <br>";
+$lekker->set_weight_kg(1);
+echo $lekker->weight_kg() . "<br>";
+echo $lekker->weight_lbs() . "<br>";
+echo "</p>";
+
+echo "<p>";
+echo "Set weight using lbs<br />";
+$lekker->set_weight_lbs(2);
+echo $lekker->weight_kg() . "<br />";
+echo $lekker->weight_lbs() . "<br />";
+echo "</p>";
+
+?>
