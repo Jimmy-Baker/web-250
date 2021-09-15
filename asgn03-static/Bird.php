01b49b49d6eefa8abb8b56524eb188fb02970119
Jimmy <baker.jimmy@gmail.com>
2021-09-15 14:54:13 -0400

    assign 3 completed?

--- asgn03-static/Bird.php
+++ asgn03-static/Bird.php
@@ -1,31 +1,38 @@
 <?php
 
 class Bird {
-    var $habitat;
-    var $food;
-    var $nesting = "tree";
-    var $conservation;
-    var $song = "chirp";
-    var $flying = "yes";
+    public $habitat;
+    public $food;
+    public $nesting = "tree";
+    public $conservation;
+    public $song = "chirp";
+    public $flying = "yes";
+    
+    public static $instance_count = 0;
+    public static $egg_num = 0;
 
-    function can_fly() {
-        if ( $this->flying == "yes" ) {
-            $flying_string = "can fly";
-        } else {
-            $flying_string = "is stuck on the ground";
-        }
+    public function can_fly() {
+        $flying_string = ($this->flying == 'yes') ? 'can fly' : 'is stuck on the ground';
         return  $flying_string ;
+    }
+    
+    public static function create() {
+        $class = get_called_class();
+        $object = new $class;
+        static::$instance_count++;
+        return $object;
     }
 }
 
 class YellowBelliedFlyCatcher extends Bird {
-    var $name = "yellow-bellied flycatcher";
-    var $diet = "mostly insects.";
-    var $song = "flat chilk";
+    public $name = "yellow-bellied flycatcher";
+    public $diet = "mostly insects.";
+    public $song = "flat chilk";
+    public static $egg_num = "3-4, sometimes 5";
 }
 
 class Kiwi extends Bird {
-    var $name = "kiwi";
-    var $diet = "omnivorous";
-    var $flying = "no";
+    public $name = "kiwi";
+    public $diet = "omnivorous";
+    public $flying = "no";
 }
--- asgn03-static/index.php
+++ asgn03-static/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>asgn02 Inheritance</title>
+    <title>asgn03 Inheritance</title>
 </head>
 <body>
 <h1>Inheritance Examples</h1>
@@ -11,16 +11,22 @@
 <?php 
     include 'Bird.php';
     
-    $bird = new Bird;
+    echo Bird::$instance_count  . " birds exist.<br>";
+    
+    $bird = Bird::create();
     echo '<p>The generic song of any bird is "' . $bird->song . '".</p>';
 
-    $fly_catcher = new YellowBelliedFlyCatcher;
+    $fly_catcher = YellowBelliedFlyCatcher::create();
     echo '<p>The song of the ' . $fly_catcher->name . ' on breeding grounds is "' . $fly_catcher->song . '".</p>';
 
-    $kiwi = new Kiwi;
+    $kiwi = Kiwi::create();
     $kiwi->flying = "no";
     echo "<p>The " . $fly_catcher->name . " " . $fly_catcher->can_fly() . ".</p>";
-    echo "<p>The " . $kiwi->name . " " . $kiwi->can_fly() . ".</p>";    
+    echo "<p>The " . $kiwi->name . " " . $kiwi->can_fly() . ".</p>";
+    
+    echo Bird::$instance_count . " birds exist.<br>";
+    echo YellowBelliedFlyCatcher::$instance_count . " Yellow Bellied Flycatchers exist.<br>";
+    echo Kiwi::$instance_count . " birds exist.<br>";
 
 ?>
     </body>
