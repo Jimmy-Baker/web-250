51fbcf534a33782cbcf45913997fa53e867a5621
Jimmy <baker.jimmy@gmail.com>
2021-09-21 10:07:12 -0400

    working on assign4

---
+++ assign04/private/classes/bird.class.php
@@ -0,0 +1,76 @@
+<?php
+
+class Bicycle {
+
+  public $habitat;
+  public $food;
+  public $nesting = "tree";
+  public $conservation;
+  public $song = "chirp";
+  public $flying = "yes";
+    
+  public static $instance_count = 0;
+  public static $egg_num = 0;
+  
+  public const CONSERVATION = ['Unknown','Low', 'Moderate','High', 'Extreme'];
+
+  public function can_fly() {
+    $flying_string = ($this->flying == 'yes') ? 'can fly' : 'is stuck on the ground';
+    return  $flying_string ;
+}
+
+public static function create() {
+    $class = get_called_class();
+    $object = new $class;
+    static::$instance_count++;
+    return $object;
+}
+  public function __construct($args=[]) {
+    //$this->brand = isset($args['brand']) ? $args['brand'] : '';
+    $this->brand = $args['brand'] ?? '';
+    $this->model = $args['model'] ?? '';
+    $this->year = $args['year'] ?? '';
+    $this->category = $args['category'] ?? '';
+    $this->color = $args['color'] ?? '';
+    $this->description = $args['description'] ?? '';
+    $this->gender = $args['gender'] ?? '';
+    $this->price = $args['price'] ?? 0;
+    $this->weight_kg = $args['weight_kg'] ?? 0.0;
+    $this->condition_id = $args['condition_id'] ?? 3;
+
+    // Caution: allows private/protected properties to be set
+    // foreach($args as $k => $v) {
+    //   if(property_exists($this, $k)) {
+    //     $this->$k = $v;
+    //   }
+    // }
+  }
+
+  public function weight_kg() {
+    return number_format($this->weight_kg, 2) . ' kg';
+  }
+
+  public function set_weight_kg($value) {
+    $this->weight_kg = floatval($value);
+  }
+
+  public function weight_lbs() {
+    $weight_lbs = floatval($this->weight_kg) * 2.2046226218;
+    return number_format($weight_lbs, 2) . ' lbs';
+  }
+
+  public function set_weight_lbs($value) {
+    $this->weight_kg = floatval($value) / 2.2046226218;
+  }
+
+  public function condition() {
+    if($this->condition_id > 0) {
+      return self::CONDITION_OPTIONS[$this->condition_id];
+    } else {
+      return "Unknown";
+    }
+  }
+
+}
+
+?>
--- assign04/public/birds.php
+++ assign04/public/birds.php
@@ -8,7 +8,7 @@
   <div id="page">
     <div class="intro">
       <img class="inset" src="<?php echo url_for('/images/AdobeStock_55807979_thumb.jpeg') ?>" />
-      <h2>Our Inventory of Used Bicycles</h2>
+      <h2>These are our birds</h2>
       <p>Choose the bike you love.</p>
       <p>We will deliver it to your door and let you try it before you buy it.</p>
     </div>
