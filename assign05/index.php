51447f4ef3deec3e9b67390eed90713aaf6f6afd
Jimmy <baker.jimmy@gmail.com>
2021-10-07 00:56:51 -0400

    fixes structure of pages

---
+++ assign05/index.php
@@ -0,0 +1,52 @@
+<?php
+
+include 'connect.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Assignment 5</title>
+    <meta name="description" content="For WEB-250">
+    <meta name="author" content="Jimmy Baker">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/skeleton.css">
+    <link rel="icon" type="image/png" href="/images/favicon.png">
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="one-half column" style="margin-top: 25%">
+          <h4>Assignment 05</h4>
+          <nav>
+            <h6>Named Parameters</h6>
+            <ul>
+              <li><a href="named-parameters/query1.php">Query 1</a></li>
+              <li><a href="named-parameters/query2.php">Query 2</a></li>
+              <li><a href="named-parameters/query3.php">Query 3</a></li>
+              <li><a href="named-parameters/query4.php">Query 4</a></li>
+              <li><a href="named-parameters/query5.php">Query 5</a></li>
+            </ul>
+            <h6>PDO-tutorial</h6>
+            <ul>
+              <li><a href="pdo-tutorial/delete.php">Delete</a></li>
+              <li><a href="pdo-tutorial/fetch.php">Fetch</a></li>
+              <li><a href="pdo-tutorial/fetchall.php">Fetch-All</a></li>
+              <li><a href="pdo-tutorial/insert.php">Insert</a></li>
+              <li><a href="pdo-tutorial/named-parameters.php">Named Parameters</a></li>
+              <li><a href="pdo-tutorial/prepared-statements.php">Prepared Statements</a></li>
+              <li><a href="pdo-tutorial/query.php">Query</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </body>
+
+</html>
