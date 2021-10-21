db600f338175458cb7c90d927dc3bd791cea2488
Jimmy <baker.jimmy@gmail.com>
2021-10-21 08:34:13 -0400

    moves error messages to below the form

--- assign07i/public/staff/bicycles/new.php
+++ assign07i/public/staff/bicycles/new.php
@@ -34,11 +34,11 @@ if(is_post_request()) {
   <div class="bicycle new">
     <h1>Create Bicycle</h1>
 
-    <?php echo display_errors($bicycle->errors); ?>
 
     <form action="<?php echo url_for('/staff/bicycles/new.php'); ?>" method="post">
 
       <?php include('form_fields.php'); ?>
+      <?php echo display_errors($bicycle->errors); ?>
 
       <div id="operations">
         <input type="submit" value="Create Bicycle" />
